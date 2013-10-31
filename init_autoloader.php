a7509d5e9815d5071f695002dccbd07df8784445
trivan <trivan@trivan-code>
2013-10-31 16:14:41 +0700

    edit code

--- init_autoloader.php
+++ init_autoloader.php
@@ -23,8 +23,8 @@ if (file_exists('vendor/autoload.php')) {
 
 $zf2Path = false;
 
-if (is_dir('vendor/zendframework/zendframework/library')) {
-    $zf2Path = 'vendor/zendframework/zendframework/library';
+if (is_dir('vendor/ZF2/library')) {
+    $zf2Path = 'vendor/ZF2/library';
 } elseif (getenv('ZF2_PATH')) {      // Support for ZF2_PATH environment variable or git submodule
     $zf2Path = getenv('ZF2_PATH');
 } elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
